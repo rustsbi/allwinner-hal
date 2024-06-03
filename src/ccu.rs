96a17534089fe901c04557d6c6bb7c0499fbc5c1
Zhouqi Jiang <luojia@hust.edu.cn>
2024-06-03 14:05:42 +0800

    ccu: use volatile register structure `RW` for register representation
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- src/ccu.rs
+++ src/ccu.rs
@@ -2,7 +2,7 @@
 
 use crate::ccu;
 use crate::time::Hz;
-use core::cell::UnsafeCell;
+use volatile_register::RW;
 
 /// Clock configuration on current SoC.
 #[derive(Debug)]
@@ -18,13 +18,13 @@ pub struct Clocks {
 pub struct RegisterBlock {
     _reserved0: [u32; 579],
     /// 0x90c - UART Bus Gating Reset register.
-    pub uart_bgr: UART_BGR,
+    pub uart_bgr: RW<UartBusGating>,
     _reserved1: [u32; 12],
     /// 0x940..=0x944 - SPI0 Clock Register and SPI1 Clock Register.
-    pub spi_clk: [SPI_CLK; 2],
+    pub spi_clk: [RW<SpiClock>; 2],
     _reserved2: [u32; 9],
     /// 0x96c - SPI Bus Gating Reset register.
-    pub spi_bgr: SPI_BGR,
+    pub spi_bgr: RW<SpiBusGating>,
 }
 
 /// Clock divide factor N.
@@ -41,215 +41,141 @@ pub enum FactorN {
 }
 
 /// UART Bus Gating Reset register.
-#[allow(non_camel_case_types)]
-#[repr(C)]
-pub struct UART_BGR(UnsafeCell<u32>);
-
-mod uart_bgr {
-    use super::UART_BGR;
-
-    impl UART_BGR {
-        /// Write settings to UART bus gating register.
-        #[inline]
-        pub fn write(&self, val: UartBusGating) {
-            unsafe { self.0.get().write_volatile(val.0) }
-        }
-
-        /// Read settings from UART bus gating register.
-        #[inline]
-        pub fn read(&self) -> UartBusGating {
-            UartBusGating(unsafe { self.0.get().read_volatile() })
-        }
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct UartBusGating(u32);
+impl UartBusGating {
+    /// Disable clock gate for UART `I`.
+    #[inline]
+    pub const fn gate_mask<const I: usize>(self) -> Self {
+        Self(self.0 & !(1 << I))
     }
-
-    /// Structure representation of UART bus gating register.
-    #[repr(transparent)]
-    pub struct UartBusGating(u32);
-
-    impl UartBusGating {
-        /// Disable clock gate for UART `I`.
-        #[inline]
-        pub const fn gate_mask<const I: usize>(self) -> Self {
-            Self(self.0 & !(1 << I))
-        }
-        /// Enable clock gate for UART `I`.
-        #[inline]
-        pub const fn gate_pass<const I: usize>(self) -> Self {
-            Self(self.0 | (1 << I))
-        }
-        /// Assert reset signal for UART `I`.
-        #[inline]
-        pub const fn assert_reset<const I: usize>(self) -> Self {
-            Self(self.0 & !(1 << (I + 16)))
-        }
-        /// Deassert reset signal for UART `I`.
-        #[inline]
-        pub const fn deassert_reset<const I: usize>(self) -> Self {
-            Self(self.0 | (1 << (I + 16)))
-        }
+    /// Enable clock gate for UART `I`.
+    #[inline]
+    pub const fn gate_pass<const I: usize>(self) -> Self {
+        Self(self.0 | (1 << I))
+    }
+    /// Assert reset signal for UART `I`.
+    #[inline]
+    pub const fn assert_reset<const I: usize>(self) -> Self {
+        Self(self.0 & !(1 << (I + 16)))
+    }
+    /// Deassert reset signal for UART `I`.
+    #[inline]
+    pub const fn deassert_reset<const I: usize>(self) -> Self {
+        Self(self.0 | (1 << (I + 16)))
     }
 }
-
-pub use uart_bgr::UartBusGating;
 
 /// SPI Clock Register.
-#[repr(C)]
-#[allow(non_camel_case_types)]
-pub struct SPI_CLK(UnsafeCell<u32>);
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct SpiClock(u32);
 
-mod spi_clk {
-    use super::{FactorN, SPI_CLK};
-
-    impl SPI_CLK {
-        /// Write settings to SPI clock register.
-        #[inline]
-        pub fn write(&self, val: SpiClock) {
-            unsafe { self.0.get().write_volatile(val.0) }
-        }
-
-        /// Read settings from SPI clock register.
-        #[inline]
-        pub fn read(&self) -> SpiClock {
-            SpiClock(unsafe { self.0.get().read_volatile() })
+impl SpiClock {
+    const CLK_SRC_SEL: u32 = 0x7 << 24;
+    const FACTOR_N: u32 = 0x3 << 8;
+    const FACTOR_M: u32 = 0xf << 0;
+    /// Get SPI clock source.
+    #[inline]
+    pub const fn clock_source(self) -> SpiClockSource {
+        match (self.0 & Self::CLK_SRC_SEL) >> 24 {
+            0x0 => SpiClockSource::Hosc,
+            0x1 => SpiClockSource::PllPeri1x,
+            0x2 => SpiClockSource::PllPeri2x,
+            0x3 => SpiClockSource::PllAudio1Div2,
+            0x4 => SpiClockSource::PllAudio1Div5,
+            _ => panic!("impossible clock source"),
         }
     }
-
-    /// Structure representation of SPI clock register.
-    #[repr(transparent)]
-    pub struct SpiClock(u32);
-
-    impl SpiClock {
-        const CLK_SRC_SEL: u32 = 0x7 << 24;
-        const FACTOR_N: u32 = 0x3 << 8;
-        const FACTOR_M: u32 = 0xf << 0;
-
-        /// Get SPI clock source.
-        #[inline]
-        pub const fn clock_source(self) -> ClockSource {
-            match (self.0 & Self::CLK_SRC_SEL) >> 24 {
-                0x0 => ClockSource::Hosc,
-                0x1 => ClockSource::PllPeri1x,
-                0x2 => ClockSource::PllPeri2x,
-                0x3 => ClockSource::PllAudio1Div2,
-                0x4 => ClockSource::PllAudio1Div5,
-                _ => panic!("impossible clock source"),
-            }
-        }
-        /// Set SPI clock source.
-        #[inline]
-        pub const fn set_clock_source(self, val: ClockSource) -> Self {
-            let val = match val {
-                ClockSource::Hosc => 0x0,
-                ClockSource::PllPeri1x => 0x1,
-                ClockSource::PllPeri2x => 0x2,
-                ClockSource::PllAudio1Div2 => 0x3,
-                ClockSource::PllAudio1Div5 => 0x4,
-            };
-            Self((self.0 & !Self::CLK_SRC_SEL) | (val << 24))
-        }
-        /// Get SPI clock divide factor N.
-        #[inline]
-        pub const fn factor_n(self) -> FactorN {
-            match (self.0 & Self::FACTOR_N) >> 8 {
-                0 => FactorN::N1,
-                1 => FactorN::N2,
-                2 => FactorN::N4,
-                3 => FactorN::N8,
-                _ => unreachable!(),
-            }
-        }
-        /// Set SPI clock divide factor N.
-        #[inline]
-        pub const fn set_factor_n(self, val: FactorN) -> Self {
-            let val = match val {
-                FactorN::N1 => 0,
-                FactorN::N2 => 1,
-                FactorN::N4 => 2,
-                FactorN::N8 => 3,
-            };
-            Self((self.0 & !Self::FACTOR_N) | (val << 8))
-        }
-        /// Get SPI clock divide factor M.
-        #[inline]
-        pub const fn factor_m(self) -> u8 {
-            (self.0 & Self::FACTOR_M) as u8
-        }
-        /// Set SPI clock divide factor M.
-        #[inline]
-        pub const fn set_factor_m(self, val: u8) -> Self {
-            Self((self.0 & !Self::FACTOR_M) | val as u32)
+    /// Set SPI clock source.
+    #[inline]
+    pub const fn set_clock_source(self, val: SpiClockSource) -> Self {
+        let val = match val {
+            SpiClockSource::Hosc => 0x0,
+            SpiClockSource::PllPeri1x => 0x1,
+            SpiClockSource::PllPeri2x => 0x2,
+            SpiClockSource::PllAudio1Div2 => 0x3,
+            SpiClockSource::PllAudio1Div5 => 0x4,
+        };
+        Self((self.0 & !Self::CLK_SRC_SEL) | (val << 24))
+    }
+    /// Get SPI clock divide factor N.
+    #[inline]
+    pub const fn factor_n(self) -> FactorN {
+        match (self.0 & Self::FACTOR_N) >> 8 {
+            0 => FactorN::N1,
+            1 => FactorN::N2,
+            2 => FactorN::N4,
+            3 => FactorN::N8,
+            _ => unreachable!(),
         }
     }
-
-    /// SPI clock source.
-    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-    pub enum ClockSource {
-        /// HOSC.
-        Hosc,
-        /// Peripheral PLL (1x).
-        PllPeri1x,
-        /// Peripheral PLL (2x).
-        PllPeri2x,
-        /// Audio PLL (div 2).
-        PllAudio1Div2,
-        /// Audio PLL (div 5).
-        PllAudio1Div5,
+    /// Set SPI clock divide factor N.
+    #[inline]
+    pub const fn set_factor_n(self, val: FactorN) -> Self {
+        let val = match val {
+            FactorN::N1 => 0,
+            FactorN::N2 => 1,
+            FactorN::N4 => 2,
+            FactorN::N8 => 3,
+        };
+        Self((self.0 & !Self::FACTOR_N) | (val << 8))
+    }
+    /// Get SPI clock divide factor M.
+    #[inline]
+    pub const fn factor_m(self) -> u8 {
+        (self.0 & Self::FACTOR_M) as u8
+    }
+    /// Set SPI clock divide factor M.
+    #[inline]
+    pub const fn set_factor_m(self, val: u8) -> Self {
+        Self((self.0 & !Self::FACTOR_M) | val as u32)
     }
 }
 
-pub use spi_clk::{ClockSource as SpiClockSource, SpiClock};
+/// SPI clock source.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SpiClockSource {
+    /// HOSC.
+    Hosc,
+    /// Peripheral PLL (1x).
+    PllPeri1x,
+    /// Peripheral PLL (2x).
+    PllPeri2x,
+    /// Audio PLL (div 2).
+    PllAudio1Div2,
+    /// Audio PLL (div 5).
+    PllAudio1Div5,
+}
 
 /// SPI Bus Gating Reset register.
-#[allow(non_camel_case_types)]
-#[repr(C)]
-pub struct SPI_BGR(UnsafeCell<u32>);
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct SpiBusGating(u32);
 
-mod spi_bgr {
-    use super::SPI_BGR;
-
-    impl SPI_BGR {
-        /// Write settings to SPI bus gating register.
-        #[inline]
-        pub fn write(&self, val: SpiBusGating) {
-            unsafe { self.0.get().write_volatile(val.0) }
-        }
-
-        /// Read settings from SPI bus gating register.
-        #[inline]
-        pub fn read(&self) -> SpiBusGating {
-            SpiBusGating(unsafe { self.0.get().read_volatile() })
-        }
+impl SpiBusGating {
+    /// Disable clock gate for SPI `I`.
+    #[inline]
+    pub const fn gate_mask<const I: usize>(self) -> Self {
+        Self(self.0 & !(1 << I))
     }
-
-    /// Structure representation of SPI bus gating register.
-    #[repr(transparent)]
-    pub struct SpiBusGating(u32);
-
-    impl SpiBusGating {
-        /// Disable clock gate for SPI `I`.
-        #[inline]
-        pub const fn gate_mask<const I: usize>(self) -> Self {
-            Self(self.0 & !(1 << I))
-        }
-        /// Enable clock gate for SPI `I`.
-        #[inline]
-        pub const fn gate_pass<const I: usize>(self) -> Self {
-            Self(self.0 | (1 << I))
-        }
-        /// Assert reset signal for SPI `I`.
-        #[inline]
-        pub const fn assert_reset<const I: usize>(self) -> Self {
-            Self(self.0 & !(1 << (I + 16)))
-        }
-        /// Deassert reset signal for SPI `I`.
-        #[inline]
-        pub const fn deassert_reset<const I: usize>(self) -> Self {
-            Self(self.0 | (1 << (I + 16)))
-        }
+    /// Enable clock gate for SPI `I`.
+    #[inline]
+    pub const fn gate_pass<const I: usize>(self) -> Self {
+        Self(self.0 | (1 << I))
+    }
+    /// Assert reset signal for SPI `I`.
+    #[inline]
+    pub const fn assert_reset<const I: usize>(self) -> Self {
+        Self(self.0 & !(1 << (I + 16)))
+    }
+    /// Deassert reset signal for SPI `I`.
+    #[inline]
+    pub const fn deassert_reset<const I: usize>(self) -> Self {
+        Self(self.0 | (1 << (I + 16)))
     }
 }
-
-pub use spi_bgr::SpiBusGating;
 
 /// Peripheral that can be clock gated by CCU.
 pub trait ClockGate {
