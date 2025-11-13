40d7a38785b8cb38bbe467a60b9f78ed9cb3c01e
Zhouqi Jiang <luojia@openatom.club>
2025-11-13 11:51:47 +0800

    feat: add blinky example, set `cfg-if` version to 1.0
    Signed-off-by: Zhouqi Jiang <luojia@openatom.club>

---
+++ examples/nezha-d1/src/bin/blinky.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use allwinner_hal::prelude::*;
+use allwinner_rt::{Clocks, Peripherals, entry};
+use panic_halt as _;
+
+#[entry]
+fn main(p: Peripherals, _c: Clocks) {
+    // light up led
+    let mut pb5 = p.gpio.pb5.into_output();
+    pb5.set_high().unwrap();
+    let mut pc1 = p.gpio.pc1.into_output();
+    pc1.set_high().unwrap();
+}
