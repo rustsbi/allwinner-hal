04ccf2512151b32b50aeebf602d26eff27561fa6
Zhouqi Jiang <luojia@openatom.club>
2025-11-13 10:26:36 +0800

    feat(rt): initialize floating point unit and enable caches on start function
    Signed-off-by: Zhouqi Jiang <luojia@openatom.club>

--- allwinner-rt/src/lib.rs
+++ allwinner-rt/src/lib.rs
@@ -49,27 +49,42 @@ unsafe extern "C" fn start() -> ! {
     #[unsafe(link_section = ".bss.uninit")]
     static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
     core::arch::naked_asm!(
+        // Disable interrupt
+        "csrw   mie, zero",
         // Enable T-Head ISA extension
         "li     t1, 1 << 22",
         "csrs   0x7C0, t1",
+        // Enable T-Head caches
+        "li     t0, 0x70013
+        csrw    0x7C2, t0
+        li      t0, 0x11ff
+        csrw    0x7C1, t0
+        li      t0, 0x638000
+        csrs    0x7C0, t0
+        li      t0, 0x16e30c
+        csrw    0x7C5, t0",
         // Invalidate instruction and data cache, branch history table
         // and branch target buffer table
         "li     t1, 0x30013",
         "csrs   0x7C2, t1",
-        // Disable interrupt
-        "csrw   mie, zero",
         // Prepare programming language stack
         "la     sp, {stack}
         li      t0, {stack_size}
         add     sp, sp, t0",
-            // Clear `.bss` section
-            "la     t1, sbss
+        // Clear `.bss` section
+        "la     t1, sbss
         la      t2, ebss
     3:  bgeu    t1, t2, 3f
         sd      zero, 0(t1)
         addi    t1, t1, 8
         j       3b
     3:  ",
+        // Enable floating point unit
+        "li     t0, 0x4000",
+        "li     t1, 0x2000",
+        "csrrc  x0, mstatus, t0",
+        "csrrs  x0, mstatus, t1",
+        "fscsr  x0",
         // Start Rust main function
         "call   {main}",
         // Platform halt if main function returns
