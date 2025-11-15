0b45544177419da1d1184443c472f0d2b2cab677
Zhouqi Jiang <luojia@openatom.club>
2025-11-15 13:05:43 +0800

    feat(rt): program halt if main function returns in thead_c906 platform
    Signed-off-by: Zhouqi Jiang <luojia@openatom.club>

--- allwinner-rt/src/arch/thead_c906.rs
+++ allwinner-rt/src/arch/thead_c906.rs
@@ -51,16 +51,29 @@ pub unsafe extern "C" fn start() -> ! {
         // Start Rust main function
         "call   {main}",
         // Platform halt if main function returns
-    "3: wfi
-        j       3b",
+        "call   {thead_c906_halt}",
         stack      =   sym STACK,
         stack_size = const STACK_SIZE,
         init_floating_point = sym init_floating_point,
         main       =   sym main,
+        thead_c906_halt = sym thead_c906_halt,
     )
 }
 
 #[cfg(not(any(all(feature = "d1", target_arch = "riscv64"), doc)))]
 pub unsafe extern "C" fn start() -> ! {
     unimplemented!()
+}
+
+#[unsafe(naked)]
+unsafe extern "C" fn thead_c906_halt() -> ! {
+    core::arch::naked_asm!(
+        "li     x3, 0x20aaa
+        csrs    mie, x3
+        csrci   mstatus, 0x8
+        csrci   0x7C5, 0x4
+        .insn i 0x0B, 0, x0, x0, 0x001
+        csrci   0x7C1, 0x2
+        wfi",
+    )
 }
