8c39a2f2d21d255eb56cfa94e697c64a704616d5
Zhouqi Jiang <luojia@rustsbi.com>
2025-11-20 17:41:55 +0800

    fix(hal): add ccu initialization in uart new function
    Signed-off-by: Zhouqi Jiang <luojia@rustsbi.com>

--- examples/nezha-d1/src/bin/uart.rs
+++ examples/nezha-d1/src/bin/uart.rs
@@ -6,6 +6,13 @@ use allwinner_rt::{Clocks, Peripherals, entry};
 
 #[entry]
 fn main(p: Peripherals, c: Clocks) {
+    // TODO make it clean
+    unsafe {
+        p.ccu
+            .uart_bgr
+            .modify(|v| v.gate_pass::<0>().deassert_reset::<0>())
+    };
+
     let pads = (p.gpio.pb8, p.gpio.pb9);
     let mut serial = p.uart0.serial(pads, Config::default(), &c);
 
